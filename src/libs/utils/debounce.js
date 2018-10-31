8cc4976f17d8451798af2cdd16c8ec7e1c794c40
sunchengbin <sunchengbin@weidian.com>
2018-10-31 13:55:34 +0800

    添加debounce用例

--- src/libs/utils/debounce.js
+++ src/libs/utils/debounce.js
@@ -1,6 +1,10 @@
 /*
  * 防抖（以最后一次事件的触发时间为起始时间，去不断推移事件的执行）
  * 应用场景（例如：避免快速点击造成的多次提交、降低input检索频次）
+ * 用例：
+ * debounce(function () {
+ *  console.log(this.name)
+ * }, 1000)
  */
 function debounce (callback, wait = 300) {
   let timeout, timestamp, context, args
