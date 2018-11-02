6672a527bd2bd7bdda4d464226439faefc5d922e
sunchengbin <sunchengbin@weidian.com>
2018-11-02 13:12:08 +0800

    框架提交

---
+++ src/views/home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <h1>svg组件的引入</h1>
+    <div @click="clickBtn">
+      <svg-icon icon-class="vip"/>
+    </div>
+    <div class="btn">
+      {{btnTxt}}
+    </div>
+  </div>
+</template>
+
+<script>
+import {
+  utils
+} from '@/libs/interfaces'
+export default {
+  name: 'home',
+  data () {
+    return {
+      btnTxt: '按钮'
+    }
+  },
+  methods: {
+    clickBtn: utils.throttle(function () {
+      console.log(this.btnTxt)
+      console.log(`now is ${Date.now()}`)
+      this.$toast({
+        message: 'message'
+      })
+    }, 1000)
+  }
+}
+</script>
