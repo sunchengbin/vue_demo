5b276a674824d4c02342f75865afd7797f17f80f
sunchengbin <sunchengbin@weidian.com>
2018-11-02 11:07:06 +0800

    添加app.vue

---
+++ src/app.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/help">Help</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  text-align: center;
+}
+#nav {
+  @include px2rem(padding, 30);
+  a {
+    font-weight: bold;
+    &.router-link-exact-active {
+      color: $active_font_color;
+    }
+  }
+}
+</style>
